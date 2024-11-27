6b59164fa258bec95a263bd302d99a74f1826878
Anthony Pena <anthony.pena@outlook.fr>
2024-11-28 00:26:23 +0100

    add POST /api/rooms

--- backend/main.ts
+++ backend/main.ts
@@ -1,21 +1,33 @@
-import { match } from 'ts-pattern';
+import { match } from "ts-pattern";
+
+const ROOM_FILE_PREFIX = "chat__room__";
 
 export function server(port: number) {
   return Deno.serve({ port }, (req) => {
-    const request = req as Request & { uri: URL }
+    const request = req as Request & { uri: URL };
     request.uri = new URL(req.url);
     return match(request)
-      .with({ method: 'GET', uri: { pathname: '/echo' } }, async () => {
-        const echo = {
-          body: await req.text(),
-          headers: req.headers,
-          url: req.url,
-          method: req.method,
-        };
-        return res(JSON.stringify(echo))
-      })
-      .otherwise(() => res('', { status: 404 }));
+      .with({ method: "POST", uri: { pathname: "/api/rooms" } }, createRoom)
+      .otherwise(() => res("", { status: 404 }));
   });
+}
+
+async function createRoom(req: Request): Promise<Response> {
+  let label: string;
+  try {
+    const body = await req.json();
+    if (body?.label == undefined || !body.label.match(/^[a-zA-Z0-9 ]+$/)) {
+      throw new Error("Invalid label");
+    }
+    label = body.label;
+  } catch (err) {
+    console.error(err);
+    return res("Invalid payload", { status: 400 });
+  }
+  const roomFile = await Deno.makeTempFile({ prefix: ROOM_FILE_PREFIX });
+  const [, roomId] = roomFile.split(ROOM_FILE_PREFIX);
+  Deno.writeTextFile(roomFile, JSON.stringify({ label, messages: [] }));
+  return res(JSON.stringify({ id: roomId, label }), { status: 201 });
 }
 
 function res(body?: BodyInit | null, init?: ResponseInit): Response {
--- backend/main_test.ts
+++ backend/main_test.ts
@@ -1,6 +1,51 @@
-import { assertEquals } from "@std/assert";
-import { add } from "./main.ts";
+import { assertEquals, assertMatch } from "@std/assert";
+import { afterAll, afterEach, beforeAll, describe, it } from "@std/testing/bdd";
+import { server } from "./main.ts";
 
-Deno.test(function addTest() {
-  assertEquals(add(2, 3), 5);
+describe("Chat", () => {
+  let api: ReturnType<typeof server>;
+  let response: Response;
+
+  beforeAll(async () => {
+    api = await server(0);
+  });
+
+  afterAll(async () => {
+    await api?.shutdown();
+  });
+
+  afterEach(async () => {
+    if (response) {
+      if (!response.bodyUsed) {
+        // ensure body is consumed to avoir memory leak
+        await response.arrayBuffer();
+      }
+    }
+  });
+
+  describe("POST /api/rooms", () => {
+    it("should create a new room", async () => {
+      response = await fetch(`http://localhost:${api.addr.port}/api/rooms`, {
+        method: "POST",
+        body: JSON.stringify({ label: "This is a name" }),
+      });
+      assertEquals(response.status, 201);
+      const room = await response.json();
+      assertMatch(room.id, /\w{16}/);
+      assertEquals(room.label, "This is a name");
+    });
+    (["", "+", "something with #", "//", "; SELECT", null!] satisfies string[])
+      .forEach((label) => {
+        it(`should reject invalid label: "${label}"`, async () => {
+          response = await fetch(
+            `http://localhost:${api.addr.port}/api/rooms`,
+            {
+            method: "POST",
+            body: JSON.stringify({ label }),
+            },
+          );
+          assertEquals(response.status, 400);
+        });
+      });
+  });
 });
