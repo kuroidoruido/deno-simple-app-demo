5467c9d495d17bede709a3620627d798af1b3444
Anthony Pena <anthony.pena@outlook.fr>
2024-11-27 23:09:57 +0100

    basic server

--- backend/main.ts
+++ backend/main.ts
@@ -1,8 +1,29 @@
-export function add(a: number, b: number): number {
-  return a + b;
+export function server(port: number) {
+  return Deno.serve({ port }, async (req) => {
+    const echo = {
+      body: await req.text(),
+      headers: req.headers,
+      url: req.url,
+      method: req.method,
+    };
+    return res(JSON.stringify(echo));
+  });
+}
+
+function res(body?: BodyInit | null, init?: ResponseInit): Response {
+  return new Response(body, {
+    status: 200,
+    ...init,
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "*",
+      ...init?.headers,
+    },
+  });
 }
 
 // Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
 if (import.meta.main) {
-  console.log("Add 2 + 3 =", add(2, 3));
+  await server(3000);
 }
